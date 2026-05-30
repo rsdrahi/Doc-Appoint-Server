fd00e90d30df5b40febdde9a1936afa60531bb34
rsdrahi <rsdrahi13@gmail.com>
2026-05-30 14:50:30 +0600

    .gitignore file created, connect with mongodb and install cors.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+// const dns = require("node:dns");
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+dotenv.config();
 const app = express()
+app.use(cors());
 
-const PORT = 5000;
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Server is Running Fine')
