3eb16971f6ead2297debabb2020442fb17da0081
rsdrahi <rsdrahi13@gmail.com>
2026-05-30 19:52:09 +0600

    add a featured route.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const dns = require("node:dns");
 // dns.setServers(["8.8.8.8", "8.8.4.4"]);
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
@@ -23,12 +23,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db('docappoint')
+    const appointCollection = db.collection('allAppointment');
+
+    app.get('/all-appointment', async (req, res) => {
+      const cursor = appointCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/all-appointment/:id', async (req, res) => {
+      const { id } = req.params;
+      const query = {_id: new ObjectId(id)}
+      const result = await appointCollection.findOne(query)
+      res.send(result);
+    })
+
+    app.get('/featured', async (req, res) => {
+      const result = await appointCollection.find().limit(3).toArray();
+      res.send(result);
+    })
+
+
+
     
-    // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    
     // await client.close();
   }
 }
